4ffefb07da77c4a6e70db071f576de7aa4c28685
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2025-06-30 10:32:02 -0700

    make query more flexible

--- src/ephys_alignment_gui/docdb.py
+++ src/ephys_alignment_gui/docdb.py
@@ -27,7 +27,7 @@ def query_docdb_id(session_name: str) -> tuple[str, dict]:
     """
 
     response = docdb_api_client.retrieve_docdb_records(
-        filter_query={"data_description.data_level": "derived", "data_description.input_data_name": session_name,
+        filter_query={"data_description.data_level": "derived", "data_description.name": {"$regex": session_name},
                       "data_description.process_name": "sorted"}
     )
     if len(response) == 0:
