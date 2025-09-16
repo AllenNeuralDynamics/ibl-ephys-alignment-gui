da81742ddc73fa156733490fdb721d1ec5cff35c
arjunsridhar12345 <arjun.sridhar1998@gmail.com>
2025-09-16 12:09:49 -0700

    fix various errors

--- src/ephys_alignment_gui/ephys_gui_setup.py
+++ src/ephys_alignment_gui/ephys_gui_setup.py
@@ -69,7 +69,7 @@ class Setup():
         custom_metrics = []
         for metrics_key, scatter_data in self.scat_custom_metrics.items():
             scatter_metric = QtWidgets.QAction(f'Cluster Amp vs Depth vs Custom Metric {metrics_key}', self, checkable=True, checked=False)
-            scatter_metric.triggered.connect(lambda img_data=scatter_data: self.plot_scatter(img_data))
+            scatter_metric.triggered.connect(lambda: self.plot_scatter(scatter_data))
             custom_metrics.append(scatter_metric)
 
         img_fr = QtWidgets.QAction('Firing Rate', self, checkable=True, checked=True)
