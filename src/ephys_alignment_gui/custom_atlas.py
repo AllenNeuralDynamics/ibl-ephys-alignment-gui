beb47dceafd2032b44c7cfbb27433e28c8b01e0c
Galen Lynch <galen@galenlynch.com>
2025-10-08 19:54:50 -0700

    WIP

--- src/ephys_alignment_gui/custom_atlas.py
+++ src/ephys_alignment_gui/custom_atlas.py
@@ -1,93 +1,68 @@
 import logging
 
-import nibabel as nib
 import numpy as np
 import SimpleITK as sitk
-from iblatlas.atlas import ALLEN_CCF_LANDMARKS_MLAPDV_UM, BrainAtlas
+from iblatlas.atlas import BrainAtlas
 from iblatlas.regions import BrainRegions
 
 _logger = logging.getLogger(__name__)
 
 
-# This is a custom atlas class that inherits from BrainAtlas
-class CustomAtlas(BrainAtlas):
-    image = None
-    label = None
-
+class BrainAtlasAnatomical(BrainAtlas):
+    pipeline_image: sitk.Image # Pipeline image associated with this atlas for CCF conversion
     def __init__(
         self,
-        atlas_image_file=None,
-        atlas_labels_file=None,
-        bregma=None,
-        force_um=None,
-        scaling=np.array([1, 1, 1]),
-    ):
-        self.atlas_image_file = atlas_image_file
-        self.atlas_labels_file = atlas_labels_file
-        if force_um is None:
-            # dxyz = np.array(self.read_atlas_image())*np.array([1, -1, -1])*1e-6
-            dxyz = np.array(self.read_atlas_image()) * 1000
-            print("Atlas scaling", dxyz)
-            self.res_um = dxyz[0]
-            dxyz = self.res_um * 1e-6 * np.array([1, 1, 1]) * scaling
-            print("Resolution", self.res_um)
+        intensity_img: sitk.Image,
+        label_img: sitk.Image,
+        pipeline_img: sitk.Image,
+    ) -> None:
+        methods_to_check = ["GetOrigin", "GetSpacing", "GetSize", "GetDirection"]
+        for m in methods_to_check:
+            intensity_val = np.array(getattr(intensity_img, m)())
+            label_val = np.array(getattr(label_img, m)())
+            if not np.allclose(intensity_val, label_val):
+                raise ValueError(
+                    f"Intensity and label {m}() mismatch: {intensity_val} != {label_val}"
+                )
+        orientation_code = (
+            sitk.DICOMOrientImageFilter.GetOrientationFromDirectionCosines(
+                intensity_img.GetDirection()
+            )
+        )
+        if orientation_code == "SRA":
+            intensity_img_sra = intensity_img
+            label_img_sra = label_img
         else:
-            _ = self.read_atlas_image()
-            self.res_um = force_um
-            dxyz = self.res_um * 1e-6 * np.array([1, -1, -1]) * scaling
-        self.read_atlas_labels()
+            _logger.info(
+                f"Reorienting volume from {orientation_code} to SRA for consistency with IBL convention."
+            )
+            intensity_img_sra = sitk.DICOMOrient(intensity_img, "SRA")
+            label_img_sra = sitk.DICOMOrient(label_img, "SRA")
+        # SRA to SAR, with C-order. This matches IBL XYZ (RAS with C order)
+        # Allows you to have coronal slices be close in memory for visualization
+        dims2xyz = np.array([1, 0, 2])
+        xyz2dims = np.array([1, 0, 2])
+
+        # Compute dxyz
+        # Reverse to go to NP/C order, and convert from μm to m
+        # This is in the IMAGE index coordinate system
+        spacing_tup_np = np.array(intensity_img_sra.GetSpacing())[::-1] * 1e-6
+        # But BrainCoordinates wants it in the XYZ world coordinate system
+
+        spacing_tup_xyz = spacing_tup_np[dims2xyz]
+        # IBL defines origin by saying which index is at world coordinate 0,0,0
+        # We are not using bregma here, so just set to 0,0,0
+        iorigin = [0, 0, 0]
         regions = BrainRegions()
-        # _, im = ismember(self.label, regions.id)
-        # label = np.reshape(im.astype(np.uint16), self.label.shape)
-        # Make sure we're in uint16 already (avoid extra copies)
-        if self.label.dtype != np.uint16:
-            self.label = self.label.astype(np.uint16, copy=False)
-
-        # Sort region IDs (only once)
-        valid = np.sort(np.asarray(regions.id, dtype=self.label.dtype))
-
-        # Binary search lookup for all labels
-        idx = np.searchsorted(valid, self.label)
-        mask = (idx < len(valid)) & (valid[idx] == self.label)
-
-        # Apply replacement in-place
-        self.label[~mask] = np.uint16(997)
-
-        xyz2dims = np.array([0, 1, 2])  # this is the c-contiguous ordering
-        dims2xyz = np.array([0, 1, 2])
-        if bregma is None:
-            bregma = [0, 0, 0]
-        elif isinstance(bregma, str) and (bregma.lower() == "allen"):
-            bregma = ALLEN_CCF_LANDMARKS_MLAPDV_UM["bregma"] / self.res_um
+        intensity_img_sra_arr = sitk.GetArrayFromImage(intensity_img_sra)
+        label_img_sra_arr = sitk.GetArrayFromImage(label_img_sra)
         super().__init__(
-            self.image, self.label, dxyz, regions, dims2xyz=dims2xyz, xyz2dims=xyz2dims
+            intensity_img_sra_arr,
+            label_img_sra_arr,
+            spacing_tup_xyz,
+            regions,
+            iorigin=iorigin,
+            dims2xyz=dims2xyz,
+            xyz2dims=xyz2dims,
         )
-        self.label[~np.isin(self.label, regions.id)] = 997
-
-    def read_atlas_image(self):
-        if self.atlas_image_file.suffix == ".nrrd":
-            print(f"Loading nrrd file: {self.atlas_image_file.as_posix()}")
-            # Reads the
-            IMG = sitk.ReadImage(self.atlas_image_file)
-            # Convert sitk to the (ap, ml, dv) np array needed by BrainAtlas
-            self.original_image = IMG
-            self.image = np.flip(sitk.GetArrayFromImage(IMG).T, axis=(0, 2))
-            print("Shape", self.image.shape)
-            self.offset = IMG.GetOrigin()
-            self.spacing = IMG.GetSpacing()[0] * 1000
-            return IMG.GetSpacing()
-        else:  # nii.gz file
-            print(f"Loading nii.gz file: {self.atlas_image_file.as_posix()}")
-            image_lazy_loaded = nib.load(self.atlas_image_file)
-            self.image = image_lazy_loaded.dataobj
-            print("Shape", self.image.shape)
-            self.spacing = image_lazy_loaded.header.get_zooms()[0] * 1000
-            self.offset = tuple(image_lazy_loaded.affine[:3, 3])
-            return tuple(image_lazy_loaded.header.get_zooms())
-
-    def read_atlas_labels(self):
-        IMG = sitk.ReadImage(self.atlas_labels_file)
-        # Convert sitk to the (ap, ml, dv) np array needed by BrainAtlas
-        self.label = np.flip(
-            sitk.GetArrayFromImage(IMG).astype(np.int32).T, axis=(0, 2)
-        )
+        self.pipeline_image = pipeline_img
