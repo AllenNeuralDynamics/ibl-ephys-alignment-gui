43d5b04df91aedc04abdfe05ffdd145e2ea04e04
Galen Lynch <galen@galenlynch.com>
2025-10-29 09:10:47 -0700

    Do some reversions

--- src/ephys_alignment_gui/custom_atlas.py
+++ src/ephys_alignment_gui/custom_atlas.py
@@ -9,13 +9,47 @@ _logger = logging.getLogger(__name__)
 
 
 class BrainAtlasAnatomical(BrainAtlas):
-    pipeline_image: sitk.Image # Pipeline image associated with this atlas for CCF conversion
+    """
+    BrainAtlas subclass for anatomical atlases built from anatomical images.
+    In addition to the BrainAtlas intensity and label arrays, this class also
+    stores the SimpleITK images for potential further processing.
+    
+    pipeline_sitk_image: sitk.Image
+        The pipeline image associated with this atlas for CCF conversion.
+    intensity_sitk_image: sitk.Image
+        The anatomical intensity image.
+    """
+    pipeline_sitk_image: sitk.Image # Pipeline image associated with this atlas for CCF conversion
+    intensity_sitk_image: sitk.Image  # Anatomical intensity image
     def __init__(
         self,
         intensity_img: sitk.Image,
         label_img: sitk.Image,
         pipeline_img: sitk.Image,
     ) -> None:
+        """
+        Initialize the BrainAtlasAnatomical class.
+        
+        Parameters
+        ----------
+        intensity_img : sitk.Image
+            The anatomical intensity image.
+        label_img : sitk.Image
+            The label image corresponding to the anatomical image. This must be the
+            in the same space, and have the same array shape as the intensity image.
+            The labels should be the **lateralized** IBL brain region labels,
+            with the left hemisphere being the negative indices and the right
+            hemisphere being the positive indices.
+        pipeline_img : sitk.Image
+            The pipeline image associated with this atlas for CCF conversion.
+            This should have the same array shape as the intensity image, but
+            may be in a different physical space. Its physical space should be
+            the one used by the CCF-registration pipeline, and allow conversion
+            of the voxel indices to the physical domain of the pipeline-computed
+            CCF transforms.
+        """
+
+        # Validate that intensity and label images have the same shape and physical space
         methods_to_check = ["GetOrigin", "GetSpacing", "GetSize", "GetDirection"]
         for m in methods_to_check:
             intensity_val = np.array(getattr(intensity_img, m)())
@@ -24,6 +58,8 @@ class BrainAtlasAnatomical(BrainAtlas):
                 raise ValueError(
                     f"Intensity and label {m}() mismatch: {intensity_val} != {label_val}"
                 )
+
+        # Reorient to SRA if needed
         orientation_code = (
             sitk.DICOMOrientImageFilter.GetOrientationFromDirectionCosines(
                 intensity_img.GetDirection()
@@ -32,30 +68,43 @@ class BrainAtlasAnatomical(BrainAtlas):
         if orientation_code == "SRA":
             intensity_img_sra = intensity_img
             label_img_sra = label_img
+            pipeline_img_sra = pipeline_img
         else:
             _logger.info(
                 f"Reorienting volume from {orientation_code} to SRA for consistency with IBL convention."
             )
             intensity_img_sra = sitk.DICOMOrient(intensity_img, "SRA")
             label_img_sra = sitk.DICOMOrient(label_img, "SRA")
+            pipeline_img_sra = sitk.DICOMOrient(pipeline_img, "SRA")
+
         # SRA to SAR, with C-order. This matches IBL XYZ (RAS with C order)
         # Allows you to have coronal slices be close in memory for visualization
         dims2xyz = np.array([1, 0, 2])
         xyz2dims = np.array([1, 0, 2])
 
-        # Compute dxyz
-        # Reverse to go to NP/C order, and convert from μm to m
-        # This is in the IMAGE index coordinate system
-        spacing_tup_np = np.array(intensity_img_sra.GetSpacing())[::-1] * 1e-6
-        # But BrainCoordinates wants it in the XYZ world coordinate system
+        # Compute dxyz (IBL spacing) from SimpleITK spacing
+        # Reverse order to go from SimpleITK fortran order to NP/C order, and
+        # convert from mm to m. SimpleITK spacing should be in mm for these
+        # images, and is also the physical spacing of the image row-major ijk
+        # axes
+        spacing_tup_np = np.array(intensity_img_sra.GetSpacing())[::-1] * 1e-3
 
+        # But BrainCoordinates wants it in the IBL XYZ (RAS) world coordinate system
         spacing_tup_xyz = spacing_tup_np[dims2xyz]
+
         # IBL defines origin by saying which index is at world coordinate 0,0,0
         # We are not using bregma here, so just set to 0,0,0
         iorigin = [0, 0, 0]
+
+        # We can use BrainRegions from iblatlas because the labels are
+        # lateralized IBL
         regions = BrainRegions()
-        intensity_img_sra_arr = sitk.GetArrayFromImage(intensity_img_sra)
-        label_img_sra_arr = sitk.GetArrayFromImage(label_img_sra)
+
+        # Get arrays from SimpleITK images. I think there's no mutation risk
+        intensity_img_sra_arr = sitk.GetArrayViewFromImage(intensity_img_sra)
+        label_img_sra_arr = sitk.GetArrayViewFromImage(label_img_sra)
+
+        # Initialize the superclass
         super().__init__(
             intensity_img_sra_arr,
             label_img_sra_arr,
@@ -65,4 +114,8 @@ class BrainAtlasAnatomical(BrainAtlas):
             dims2xyz=dims2xyz,
             xyz2dims=xyz2dims,
         )
-        self.pipeline_image = pipeline_img
+
+        # Store the SimpleITK intensity image, and the pipeline image for use
+        # with CCF transforms
+        self.intensity_sitk_image = intensity_img_sra
+        self.pipeline_sitk_image = pipeline_img_sra
